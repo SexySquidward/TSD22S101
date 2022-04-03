e65b96f43e9538ac8aca41ad3615e6daea1d5132
Togo1101 <100743220+Togo1101@users.noreply.github.com>
2022-04-03 12:39:14 +0930

    Add files via upload

---
+++ StarFinanceMaster/InstaRichie/Models/PersonalInfoDB.cs
@@ -0,0 +1,53 @@
+// **************************************************************************
+//Start Finance - An to manage your personal finances.
+//Copyright(C) 2016  Jijo Bose
+
+//Start Finance is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//Start Finance is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Start Finance.If not, see<http://www.gnu.org/licenses/>.
+// ***************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    public class PersonalInfoDB
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonalID { get; set; }
+        
+        [NotNull]
+        public string FirstName { get; set; }
+
+        [NotNull]
+        public string LastName { get; set; }
+        
+        [NotNull]
+        public string DOB { get; set; }
+
+        [NotNull]
+        public string Gender { get; set; }
+        
+        [NotNull]
+        public string EmailAddress { get; set; }
+        
+        [NotNull]
+        public string MobilePhone { get; set; }
+        
+
+    }
+}
